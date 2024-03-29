a707d96b7fdb2b296236834953541bdb4de9e8a8
Michael Hofmann LY <Michael.Hofmann@OrangeObjects.de>
2024-03-29 20:06:49 +0100

    Improvements

--- generic/JavaStudies/src/test/java/net/michaelhofmann/javastudies/misc/medium/ShapeTest.java
+++ generic/JavaStudies/src/test/java/net/michaelhofmann/javastudies/misc/medium/ShapeTest.java
@@ -5,15 +5,8 @@
  */
 package net.michaelhofmann.javastudies.misc.medium;
 
-import net.michaelhofmann.javastudies.misc.medium.Shape.Circle;
 import net.michaelhofmann.javastudies.misc.medium.Shape.Rectangle;
-import net.michaelhofmann.javastudies.misc.medium.Shape.Triangle;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
@@ -21,28 +14,6 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class ShapeTest {
     
-    public ShapeTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
-    /**
-     * Test of calculateEdges method, of class Shape.
-     */
     @Test
     public void testCalculateEdges() {
         Object obj = new Rectangle();
